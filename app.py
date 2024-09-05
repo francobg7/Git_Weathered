218d4d30eb08fd043c7bbccdc005c718baaaf824
franco <francobg7@gmail.com>
2024-09-04 21:35:14 -0400

    Manejando la respuesta de la API para mostrar el clima y la temperatura cuando la ciudad es encontrada

--- app.py
+++ app.py
@@ -12,3 +12,11 @@ weather_data = requests.get(
 
 #haciendo la primera solicitud, la url utilizada es un endpoint 
 #el resultado de la solicitud se guarda en la variable weather_data 
+if weather_data.json()['cod'] == '404':
+    print("Ciudad no Encontrada")
+else:
+    clima = weather_data.json()['weather'][0]['description']
+    temperatura = round(weather_data.json()['main']['temp'])
+
+    print(f"El Clima en {user_input} esta: {clima}")
+    print(f"La temperatura en {user_input} es: {temperatura}ºC")
